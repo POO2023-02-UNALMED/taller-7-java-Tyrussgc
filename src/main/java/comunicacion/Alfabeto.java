4c18b1f7150b858da9906498a82c4b1b23e53ad2
Tyrussgc <141864114+Tyrussgc@users.noreply.github.com>
2023-10-09 03:20:46 -0500

    2

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -1,5 +1,37 @@
 package comunicacion;
 
-public class Alfabeto {
-
+public class Alfabeto extends Pictograma{
+    private String[] letras;
+    private String interpretacion;
+    public Alfabeto(String origen, String[] letras, String interpretacion) {
+        super(origen);
+        this.letras = letras;
+        this.interpretacion = interpretacion;
+    }
+    public String[] getLetras() {
+        return this.letras;
+    }
+    public void setLetras(String[] letras) {
+        this.letras = letras;
+    }
+    public String getInterpretacion() {
+        return this.interpretacion;
+    }
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+    public int cantidadLetras() {
+        return this.letras.length;
+    }
+    public String interpretacion() {
+        return this.interpretacion;
+    }
+    public String toString() {
+        String r = "";
+        for (int i = 0; i < this.cantidadLetras()-1; i++) {
+            r += letras[i]+", ";
+        }
+        r += letras[this.cantidadLetras()-1];
+        return r;
+    }
 }
---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,32 @@
+package comunicacion;
+
+public class Fabula extends Escrito {
+    private String ensenanza;
+    private String interpretacion;
+    public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+        super(origen, titulo, autor, paginas);
+        this.ensenanza = ensenanza;
+        this.interpretacion = interpretacion;
+    }
+    public String getEnsenanza() {
+        return ensenanza;
+    }
+    public void setEnsenanza(String ensenanza) {
+        this.ensenanza = ensenanza;
+    }
+    public String getInterpretacion() {
+        return interpretacion;
+    }
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+    public int palabrasTotales(int a) {
+        return this.getPaginas()*a*1;
+    }
+    public String interpretacion() {
+        return this.interpretacion;
+    }
+    public String toString() {
+        return this.resumen()+"\n"+this.getEnsenanza();
+    }
+}
